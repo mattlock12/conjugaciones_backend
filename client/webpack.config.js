aaea465452ca2ee4c373212b4584910bdb36b9ee
Matt Leick <mattleick@Matt-Leicks-MacBook-Pro.local>
2018-12-09 19:53:29 -0600

    docker-compose.prod.yml

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         })
     ],
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name][hash].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
